c858f57bc9b163fa3dd4c1270b8a8d67dbd10e74
user2 <rohithmailsu@gmail.com>
2024-10-14 12:10:15 +0530

    basic setup init

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// allows requests from other ports or machines. cors options can be set to restrict requests
+app.use(cors()); 
+
+// config database and .env variables
+const dotenv = require('dotenv');
+dotenv.config({path: "./config/.env"});
+
+
+// set routes
+const userDataRouter = require('./routes/userDataRouter.js');
+const taskDataRouter = require('./routes/taskDataRouter.js');
+
+app.use(userDataRouter);
+app.use(taskDataRouter);
+
+const MACHINE = process.env.MACHINE || "localhost";
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+   console.log(`Server listening on http://${MACHINE}:${PORT}`);   
+})
